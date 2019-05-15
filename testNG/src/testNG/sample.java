431dd2effe0eb90d279a6b14ee943ca8e98edfae
monishraj96 <monizraj96@gmail.com>
2019-05-15 11:42:18 +0530

    updated

---
+++ testNG/src/testNG/sample.java
@@ -0,0 +1,11 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	@Test
+private void test1() {
+	// TODO Auto-generated method stub
+System.out.println("JVDJY");
+}
+}
